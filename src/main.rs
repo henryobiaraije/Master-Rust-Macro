34bd036ddd7be22142bbf31d5791c8da3476331b
henryobiaraije <mpereere@gmail.com>
2023-03-10 10:07:56 +0100

    Added a new example of a custom declarative macro to mimic the vec! macro

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,32 @@
 fn main() {
+    let sizes = Vec::from([1, 2, 3, 4, 5]);
+    let sizes2 = [1, 2, 3, 4, 5].to_vec();
+    let sizes3 = vec![1, 2, 3, 4, 5];
+
+    let sizes4 = my_vec!(1, 2, 3, 4, 5);
+    let sizes42 = my_vec!(1.0, 2.0, 3.0, 4.0, 5.0);
+    let sizes5: Vec<i32> = my_vec!();
+
+    let mut sizes6 = Vec::new();
+    sizes6.push(1);
+    sizes6.push(2);
+    sizes6.push(3);
+    sizes6.push(4);
+    sizes6.push(5);
+
+}
+
+
+
+#[macro_export]
+macro_rules! my_vec  {
+    ($($x:expr),*) => {
+        {
+            let mut new_vec = Vec::new();
+            $(
+                new_vec.push($x);
+            )*
+            new_vec
+        }
+    }
 }
